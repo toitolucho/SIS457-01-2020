6ec8943fad57ff07daed057c088bf9178bdaf00d
Luis Antonio Molina Yampa <antoniomolina.yampa@gmail.com>
2020-03-10 18:01:24 -0400

    Plantilla de la estructura AutoBus
    Dada una seria de rutas de 10 trayectos donde en un autobus suben y bajan personas, calcular la cantidad de personas que se quedan en la ultima parada del trayecto.
    Considere las validaciones necesarias en las acciones de subir y bajar.
    Implemente las funciones, estaVacio, estaLleno, bajar y subir

---
+++ Estructura AutoBus.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+struct AutoBus{
+	float nivel_combustible;
+	int nro_pasajeros;
+	int nro_asientos;
+	int nro_asientos_ocupados;
+	string nombre_chofer;
+	string ci_chofer;
+};
+
+bool estaLleno(AutoBus a)
+{
+	return false;
+}
+
+bool estaVacio(AutoBus a)
+{
+	return false;
+}
+
+void recogerPasajeros( int nro_pasajeros, float peso_promedio, AutoBus a)
+{
+	
+}
+
+void dejarPasajeros( int nro_pasajeros, AutoBus a)
+{
+	
+}
