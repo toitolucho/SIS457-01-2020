1781e059785f8487e626b0d1eb561c207c2871db
Luis Antonio Molina Yampa <antoniomolina.yampa@gmail.com>
2020-07-22 10:59:25 -0400

    Ejemplo Listas Pilas STL

---
+++ Ejemplo Listas.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+void mostrarElementos(list<string> lista)
+{
+	for(auto  i = lista.begin(); i!= lista.end(); ++i )
+	{
+		cout<< *i <<" ";
+	}
+	cout<<endl;
+}
+
+
+int main(int argc, char *argv[]) {
+	list<string> estudiantes;
+	
+	estudiantes.push_back("fernando");
+	estudiantes.push_back("alexander");
+	estudiantes.push_front("Romina");
+	estudiantes.push_back("adolfo");
+	estudiantes.push_back("daniel");
+	estudiantes.push_back("carlos");
+	
+	mostrarElementos(estudiantes);
+	
+	if(estudiantes.empty())
+	{
+		cout<<"lista vacia"<<endl;
+	}
+	else
+		cout<<"lista con elementos"<<endl;
+	
+	
+	
+	
+	string primero, ultimo;
+	
+	primero = estudiantes.front();
+	estudiantes.pop_front();
+	
+	ultimo = estudiantes.back();
+	estudiantes.pop_back();
+	mostrarElementos(estudiantes);
+	
+	cout<<"Primer elemento "<< primero<<endl;
+	cout<<"Ultimo elemento "<< ultimo<<endl;
+	
+	estudiantes.sort();
+	estudiantes.reverse();
+	
+	mostrarElementos(estudiantes);
+	
+	
+	return 0;
+}
+
---
+++ Ejemplo Pilas.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	stack<string> pila;
+	
+	pila.push("alejandro");
+	pila.push("rodrigo");
+	pila.push("fernando");
+	
+	cout<<pila.top()<<endl;
+	
+	pila.pop();
+	
+	cout<<pila.top()<<endl;
+	
+	return 0;
+}
+
