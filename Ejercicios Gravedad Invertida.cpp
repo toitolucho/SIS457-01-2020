dda806a1b1dded664b47eaeff387bdb31e691fe8
Luis Antonio Molina Yampa <antoniomolina.yampa@gmail.com>
2020-08-04 09:13:48 -0400

    Solucion Ejercicio Gravedad Invertida
    En esta oportunidad solucionaremos un problema relalcionada al tema de Ordenación de elementos utilizando listas

---
+++ Ejercicios Gravedad Invertida.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	list<int> columnas;
+	int n;
+	cin>>n;
+	for(int i = 0; i<n; i++)
+	{
+		int a;
+		cin>>a;
+		columnas.push_back(a);
+	}
+	columnas.sort();
+	//list<int>::  j
+	for(auto j = columnas.begin(); j!=columnas.end(); ++j)
+	{
+		cout<< *j <<" ";
+	}
+	
+	
+	return 0;
+}
+
