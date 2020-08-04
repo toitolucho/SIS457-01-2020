ef6bb445499bc65e08c19cc7eab943318b71d574
Luis Antonio Molina Yampa <antoniomolina.yampa@gmail.com>
2020-08-04 08:41:11 -0400

    Ejercicio para validar una expresion
    En este ejercicio validaremos una expresión compuesta por parentesis, llave y corchetes utilizando el concepto de pilas

---
+++ Ejercicio Expresiones Pila.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+bool esvalida(string expresion)
+{
+	stack<char> pila_exp;
+	for(int i = 0; i<expresion.size(); i++)
+	{
+		char a = expresion[i];
+		if(a == '(' || a =='{' || a == '[')
+		{
+			pila_exp.push(a);
+		}
+		else
+		{
+			if(pila_exp.size() == 0)
+			{
+				return false;
+			}//{[(])}  ()()
+			char x = pila_exp.top();
+			switch (a)
+			{
+				case ')':
+					if(x == '{' || x == '[')
+						return false;
+					break;
+				case '}':
+					if(x == '('  || x == '[')
+						return false;
+					break;
+				case ']':
+					if(x == '{'  || x == '(')
+						return false;
+					break;
+			}
+			pila_exp.pop();
+					
+					
+			
+		}
+	}
+	return pila_exp.size() == 0;
+		
+}
+
+int main(int argc, char *argv[]) {
+	
+	string expresion;
+	int n;
+	cout<<"Int. Cantidad de pruebas"<<endl;
+	cin>>n;
+	while(n--)
+	{
+		cin>>expresion;
+		if(esvalida(expresion))
+			cout<<"YES"<<endl;
+		else 
+			cout<<"NO"<<endl;
+	}
+	
+	
+	return 0;
+}
+
