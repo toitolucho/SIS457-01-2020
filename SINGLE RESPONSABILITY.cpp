e448706f3ebf5390d481d5dd8543f0a28c6abbe4
Luis Antonio Molina Yampa <antoniomolina.yampa@gmail.com>
2020-08-24 10:53:16 -0400

    Patrones de Diseño Principio Single Responsability

---
+++ SINGLE RESPONSABILITY.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+class Agenda
+{
+	public : 
+		vector<string> notas;
+		int gestion;
+	
+		void anotar(string nota)
+		{
+			notas.push_back(nota);
+		}
+		
+		void borrar(string nota)
+		{
+			
+		}
+		void visualizar()
+		{
+			for(string nota: notas)
+			{
+				cout<<nota<<endl;
+			}
+		}
+		
+		
+	
+};
+
+class ManegadorAgenda
+{
+	public:
+	static void sacarRespaldo(Agenda& agenda,  string nombreArchivo)
+	{
+		ofstream ofs (nombreArchivo);
+		for(string nota: agenda.notas)
+		{
+			ofs<<nota<<endl;
+		}
+	}
+};
+
+int main(int argc, char *argv[]) {
+	
+	Agenda miAgenda;
+	miAgenda.anotar("1er Parial Programacion 01/09/2020");
+	miAgenda.anotar("2do Parial Programacion 15/09/2020");
+	miAgenda.anotar("Final Parial Programacion 30/09/2020");
+	miAgenda.anotar("Sesion de Reunion");
+	//miAgenda.sacarRespaldo("respaldo.txt");
+	
+	//miAgenda.visualizar();
+	ManegadorAgenda::sacarRespaldo(miAgenda, "respaldo2.txt");
+		
+	return 0;
+}
+
